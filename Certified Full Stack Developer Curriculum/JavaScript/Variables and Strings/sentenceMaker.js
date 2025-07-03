16c02b455a00e35361b5e9da4a29421055c21c54
mmilushev91 <milushev_1990@abv.bg>
2025-07-03 11:26:38 +0300

    Create sentenceMaker.js

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Variables and Strings/sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "ancient";
+let noun = "beast";
+let verb ="walking";
+let place = "Strange forest";
+let adjective2 = "large";
+let noun2 = "trespassers";
+
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log("First story: " + firstStory);
+
+adjective = "powerful";
+noun = "dragon";
+verb ="flying";
+place = "Molten mountain";
+adjective2 = "huge";
+noun2 = "humans";
+
+const secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
