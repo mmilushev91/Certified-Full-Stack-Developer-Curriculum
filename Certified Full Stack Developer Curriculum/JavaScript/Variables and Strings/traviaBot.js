7b0a6f8e2a93306a640c41180eecaafc6656288e
mmilushev91 <milushev_1990@abv.bg>
2025-07-03 09:47:53 +0300

    Create traviaBot.js

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Variables and Strings/traviaBot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "Telbot";
+const botLocation = "Telbotia";
+const favoriteLanguage = "Telbotski";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " comes from " + botName + "."; 
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " comes from " + botName + " and it is used by all bots from the Telbot type.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " comes from " + botName + " and it is used by all bots from the Telbot type. Also a few newer version of bots from bot type can also understand the Telbotski language";
+
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName +  " from " + botLocation + ".");
