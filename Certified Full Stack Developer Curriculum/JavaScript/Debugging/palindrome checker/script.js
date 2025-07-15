0e0583d16bd988b66b0395ccec07f0c70584414e
mmilushev91 <milushev_1990@abv.bg>
2025-07-15 12:28:46 +0300

    Update script.js

--- Certified Full Stack Developer Curriculum/JavaScript/Debugging/palindrome checker/script.js
+++ Certified Full Stack Developer Curriculum/JavaScript/Debugging/palindrome checker/script.js
@@ -1,9 +1,22 @@
 const checkButton = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
+const result = document.getElementById("result");
+
 
 checkButton.addEventListener("click", function() {
   
   if (textInput.value === "") {
     alert("Please input a value")
   }
+  const pattern = /[a-zA-Z0-9]/;
+  const textInputAlphaNum = textInput.value.split("").filter(char => pattern.test(char)).join("");
+
+  console.log(textInputAlphaNum)
+  result.textContent =  `${textInput.value} is ${isPalindrome(textInputAlphaNum) ? "" : "not"} a palindrome`;
+
 })
+
+function isPalindrome(word) {
+  const wordLowerCase = word.toLowerCase();
+  return wordLowerCase === wordLowerCase.split("").reverse().join("");
+}
